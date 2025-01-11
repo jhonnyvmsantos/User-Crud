75b5c222a202c202bd84bf4649d4ff75ea6ce19c
Jaqueline Alencar <111159660+Jaquelineralencar@users.noreply.github.com>
2025-01-11 20:03:41 -0300

    Jhonny: BD-Fix

--- project/database/config.php
+++ project/database/config.php
@@ -4,10 +4,27 @@
     define('PASSWORD', '');
     define('DATABASE', 'user_db');
 
-    $conn = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
+    $conn = new mysqli(HOST, USERNAME, PASSWORD);
 
     if ($conn->connect_error) {
         die("Erro de conexão: " . $conn->connect_error);
+    } else {
+        $sql = "CREATE DATABASE IF NOT EXISTS user_db;";
+
+        mysqli_query($conn, $sql);
+
+        $conn->select_db(DATABASE);
+
+        $sql = "
+            CREATE TABLE IF NOT EXISTS tbl_user (
+                user_id INT PRIMARY KEY AUTO_INCREMENT,
+                user_name VARCHAR(50),
+                user_email VARCHAR(100) UNIQUE,
+                user_password VARCHAR(100)
+            );
+        ";
+
+        mysqli_query($conn, $sql);
     }
 
     echo "
